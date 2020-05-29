811a4cbadec475df07174732ea89ddc89bc04e29
Riley Slayden <slaydenriley@gmail.com>
2020-05-29 15:17:19 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+
     /// counter inner text
     let counter = document.querySelector("#counter")
-    
+
     ///// runs counter every second
     let count = setInterval(function () {
-        counter.innerText++; 
-    }, 1000); 
+        counter.innerText++;
+    }, 1000);
 
-    
-    /// DOM Elements 
+
+    /// DOM Elements
     let minus = document.getElementById("-")
     let plus = document.getElementById("+")
     let love = document.getElementById("<3")
@@ -18,50 +18,50 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let submitBtn = document.querySelector("#submit")
     let commentList = document.querySelector("#list")
-    
-    
+
+
     // minus button
     minus.addEventListener("click", function(e){
         counter.innerText--;
     });
-    
+
     // plus button
     plus.addEventListener("click", function(e){
         counter.innerText++;
     });
-    
-    
+
+
     // pause button
     pause.addEventListener("click", function(e){
         if (pause.innerText === "pause"){
             pause.innerText = "resume"
             clearInterval(count)
-            
+
             plus.disabled = true;
             minus.disabled = true;
-            love.disabled = true; 
+            love.disabled = true;
             submitBtn.disabled = true;
         }
         else {
             count = setInterval(function () {
-                counter.innerText++; 
+                counter.innerText++;
             }, 1000);
             pause.innerText = "pause"
 
             plus.disabled = false;
             minus.disabled = false;
-            love.disabled = false; 
+            love.disabled = false;
             submitBtn.disabled = false;
         }
     });
-    
+
     // love button
     love.addEventListener("click", function(e){
         let obj = document.getElementById(`${counter.innerText}`);
-        obj ? obj.children[0].innerText++ : 
+        obj ? obj.children[0].innerText++ :
         likes.innerHTML += `<li id=${counter.innerText}>${counter.innerText} is liked <span id=${counter.innerText}>1</span> times.</li>`
     });
-    
+
     // submit comments // button
     submitBtn.addEventListener("click", function(e){
         e.preventDefault();
@@ -69,5 +69,5 @@ document.addEventListener("DOMContentLoaded", () => {
         commentList.innerHTML += `<li>${comment}</li>`
         document.querySelector("#comment-form").reset();
     });
-    
+
 });
